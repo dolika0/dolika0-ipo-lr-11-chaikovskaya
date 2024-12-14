b93f4cbb95413aa8ce3dbe4bac45387e496838a3
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-12-14 22:21:53 +0300

    Update airplane.py

--- transport/airplane.py
+++ transport/airplane.py
@@ -2,7 +2,7 @@ from .vehicle import Vehicle # Импорт класс Vehicle
 
 class Airplane(Vehicle):
     def __init__(self, max_altitude):
-        super().__init__(self, max_altitude)
+        super().__init__(self, max_altitude) # super() возвращает временный объект суперкласса, который позволяет вызывать его методы
         self.max_altitude = max_altitude #  максимальная высота полета (в метрах)
 
     def __str__(self):
