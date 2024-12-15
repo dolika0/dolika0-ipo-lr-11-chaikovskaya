d2f095d6d7aa96ca358759921e8914d2d0aa99c8
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-12-15 22:46:42 +0300

    Update vehicle.py

--- transport/vehicle.py
+++ transport/vehicle.py
@@ -1,5 +1,4 @@
-
-from transport.client import Client
+from .client import Client
 import random 
 
 def validation(prompt):
@@ -8,27 +7,28 @@ def validation(prompt):
         if num.isdigit():
             return int(num)
         else: 
-            return "Введите необходимую информацию числом!"
+            print("Введите необходимую информацию числом!")
 
 
 class Vehicle:
-    def __init__(self, capacity = 0, current_load = 0):
+    def __init__(self, capacity = 0):
         self.vehicle_id = random.randint(100, 10000)
-        self.capacity = validation("Введите грузоподъёмность транспорта (числом): ") # Грузоподъёмность
-        self.current_load = validation("Введите текущую загрузку(числом): ") # Текущая загрузка
-        self.clients_list = []
-
-    def load_cargo(self, client):
-        try:
-            new_weight = self.current_load + client.cargo_weight
-        except AttributeError:
-            raise AttributeError("Вы должны передать клиента в параметр функции!")
-        if new_weight > self.capacity:
-            print("Грузоподъемность превышена! Действие отменено!")
-        else:
-            self.current_load = new_weight
-            self.clients_list.append(client)
+        self.capacity = capacity  # грузоподъемность транспортного средства в тоннах
+        self.current_load = 0  # текущая загрузка транспортного средства
+        self.clients_list = []  # список клиентов, чьи грузы загружены
+    
             
     def __str__(self):
         return f"ID транспорта: {self.vehicle_id}, грузоподъёмность в тоннах: {self.capacity}, текущая загрузка: {self.current_load}"
     
+    
+    def load_cargo(self, client):
+        if not isinstance(client, Client): 
+            raise ValueError("client должен быть объектом класса Client")
+        if (self.current_load + client.cargo_weight) > self.capacity:
+            print("Превышение грузоподъемности транспортного средства")
+            exit()
+        else:
+            self.current_load = self.current_load + client.cargo_weight
+            self.clients_list.append(client)
+
