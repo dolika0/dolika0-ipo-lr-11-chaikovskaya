05b4ebc39d3af5046ed431a4b875ee0662358bc3
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-12-15 22:45:15 +0300

    Update transportCompany.py

--- transport/transportCompany.py
+++ transport/transportCompany.py
@@ -1,23 +1,40 @@
-from .vehicle import Vehicle
-from .client import Client
+from .vehicle import Vehicle # Импорт класс Vehicle
 from .airplane import Airplane
 from .van import Van
-from typing import List
+from .client import Client
 
-class TransportCompany():
-    # Инициализирует объект транспортной компании с именем name и пустыми списками для транспортных средств и клиентов
+class TransportCompany:
     def __init__(self, name):
         self.name = name
-        self.vehicles : List[Vehicle] = []
-        self.clients : List[Client] = []
+        self.vehicles = [] # список транспортных средств
+        self.clients = [] # список клиентов
 
-    def list_vehicles(self):
-        return self.vehicles
-    
-    def add_client(self, client):
+    def add_vehicle(self, vehicle): # добавляет транспортное средство
+        if not isinstance(vehicle, Vehicle): # — это условие, которое проверяет, является ли объект vehicle экземпляром класса Vehicle
+            raise ValueError("vehicle должен быть объектом класса Vehicle")
+        self.vehicles.append(vehicle)
+
+    def list_vehicles(self): # возвращает список всех транспортных средств
+        vehicles_list = []
+        for vehicle in self.vehicles:
+            vehicles_list.append(str(vehicle))
+        return vehicles_list 
+
+
+    def add_client(self, client): # добавляет клиента
         if not isinstance(client, Client):
-            raise TypeError("Клиент должен быть экземпляром.")
+            raise ValueError("client должен быть объектом класса Client")
         self.clients.append(client)
 
-    def add_vehicle(self, vehicle):
-        
+    def optimize_cargo_distribution(self): # распределяет грузы клиентов по транспортным средствам
+        vip_clients = [client for client in self.clients if client.is_vip] # создает новый список, состоящий из клиентов, у которых атрибут is_vip равен True
+        other_clients = [client for client in self.clients if not client.is_vip] # создает новый список, состоящий из клиентов, у которых атрибут is_vip равен False
+        all_clients = vip_clients + other_clients
+
+        for client in all_clients:
+            for vehicle in self.vehicles: # цикл проходит по каждому транспортному средству в списке self.vehicles
+                if vehicle.current_load + client.cargo_weight <= vehicle.capacity: # тек.загрузка + вес груза клиента <= грузоподьемность транспорта
+                    vehicle.load_cargo(client) # загружает груз клиента в транспортное средство и обновляет текущую загрузку
+                    break
+
+
